184163c616c9b8fd316946db449f312a436aca78
RajKumarMondol <raajkumarmondol@gmail.com>
2019-12-05 12:55:54 +0530

    Initial Commit for Template Files

---
+++ TopCoder.Tests/Template.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace TopCoder.Tests
+{
+    public class Template
+    {
+        [Theory]
+        [InlineData(new int[] { 0, 3 }, "Possible")]
+        public void Test(int[] input, string expectedOutput)
+        {
+
+        }
+    }
+}
---
+++ TopCoder/Template.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TopCoder
+{
+    public class Template
+    {
+    }
+}
