376c9c0371a346908d93ce69dfa54dfe2f70cdeb
RajKumarMondol <raajkumarmondol@gmail.com>
2019-12-06 10:51:27 +0530

    AdventofCode : FuelRequirement Day 01Dec2019 PartTwo

--- CompetitiveProgramming/AdventOfCode/FuelRequirement.cs
+++ CompetitiveProgramming/AdventOfCode/FuelRequirement.cs
@@ -9,14 +9,31 @@ namespace CompetitiveProgramming.AdventOfCode
             long totalFuelRequirement = 0;
             foreach (int moduleMass in moduleMasses)
             {
-                totalFuelRequirement += getFuelRequirement(moduleMass);
+                totalFuelRequirement += getFuelRequirementForMass(moduleMass);
             }
             return totalFuelRequirement;
         }
 
-        private int getFuelRequirement(int moduleMass)
+        private int getFuelRequirementForMass(int moduleMass)
         {
             return (moduleMass / 3 - 2);
+        }
+
+        public long calculateFuelForMassAndFuel(int[] moduleMasses)
+        {
+            long totalFuelRequirement = 0;
+            foreach (int moduleMass in moduleMasses)
+            {
+                totalFuelRequirement += getFuelRequirementForMassAndFuel(moduleMass);
+            }
+            return totalFuelRequirement;
+        }
+
+        private int getFuelRequirementForMassAndFuel(int moduleMassOrFuelMass)
+        {
+            int fuelMass = getFuelRequirementForMass(moduleMassOrFuelMass);
+            if (fuelMass <= 0) return 0;
+            else return (fuelMass + getFuelRequirementForMassAndFuel(fuelMass));
         }
     }
 }
